b3d3a1ee4fe591fed7c68ce0660e98e28dc63f67
discountry <331027793@qq.com>
2017-02-25 22:10:16 +0800

    dynamic-item-ready

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,27 +1,27 @@
 var sectionHeight = function() {
-  var total    = $(window).height(),
-      $section = $('section').css('height','auto');
+  var total = $(window).height(),
+    $section = $('section').css('height', 'auto');
 
   if ($section.outerHeight(true) < total) {
     var margin = $section.outerHeight(true) - $section.height();
     $section.height(total - margin - 20);
   } else {
-    $section.css('height','auto');
+    $section.css('height', 'auto');
   }
 }
 
 $(window).resize(sectionHeight);
 
-$(document).ready(function(){
-  $("section h1, section h2").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+$(document).ready(function() {
+  $("section h1, section h2").each(function() {
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '') + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id", $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, ''));
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
   $("nav ul li").on("click", "a", function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
+    $("html, body").animate({ scrollTop: position }, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
     event.preventDefault();
@@ -31,23 +31,25 @@ $(document).ready(function(){
 
   $('img').load(sectionHeight);
 
-  $('img').each(function(){
-    $(this).wrap("<a class='item' href='" + $(this).prop('src') +"'></a>")
+  $('img').each(function() {
+    $(this).wrap("<a class='item' href='" + $(this).prop('src') + "'></a>")
   });
 
-  $('.item').on('click', function() {
-    $("#lightGallery").lightGallery({ selector: '.item'});
+});
+
+$(document).ready(function() {
+  $("#lightgallery").lightGallery({
+    selector: '.item'
   });
-  
 });
 
 fixScale = function(doc) {
 
   var addEvent = 'addEventListener',
-      type = 'gesturestart',
-      qsa = 'querySelectorAll',
-      scales = [1, 1],
-      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+    type = 'gesturestart',
+    qsa = 'querySelectorAll',
+    scales = [1, 1],
+    meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
 
   function fix() {
     meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
@@ -60,3 +62,4 @@ fixScale = function(doc) {
     doc[addEvent](type, fix, true);
   }
 };
+
